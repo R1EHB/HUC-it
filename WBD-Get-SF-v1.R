e1a68ef0ba97766f1b2cbf654bd8fa2b465c242e
erik Beck <erikbeck@WahpenayoPeak.tahoma.link>
2024-07-23 14:35:29 -0400

    updated directories to write to and other fixes
    Committer: erik Beck <beck.erik@epa.gov>

--- WBD-Get-SF-v1.R
+++ WBD-Get-SF-v1.R
@@ -10,8 +10,6 @@
 # st_layers is part of SF package
 
 
-
-
 if (!require (sf)) {
    install.packages("sf", repos="https://cloud.r-project.org")
    }
@@ -30,8 +28,12 @@ library (curl)
 
 # Relative to Startup Directory
 
+# Save Current Directory
+
+startup_dir <- getwd()
+
 dir.create ("../DataFetch/")
-dir.create("../HUC-Data-Lists/")
+dir.create("./HUC-Data-Lists/")
 setwd("../DataFetch/")
 
 
@@ -117,9 +119,12 @@ HUC16DF <- st_drop_geometry(HUC16DF_SF)
 
 # Write CSV Location
 
-# Current Directory
+# change back to starting directory
+setwd(startup_dir)
 
-setwd("../HUC-Data-Lists/")
+# Then change to subdir off starting dir
+
+setwd("./HUC-Data-Lists/")
 
 # Write CSV Files
 write.csv (HUC2DF, "HUC2List.csv")
@@ -132,78 +137,6 @@ write.csv (HUC14DF, "HUC14List.csv")
 write.csv (HUC16DF, "HUC16List.csv")
 
 q()
-
-
-# ogrListLayers(gdb)
-
-# Layers of Interest:
-  # * WBDHU2
-  # * WBDHU4
-  # * WBDHU6
-  # * WBDHU8
-  # * WBDHU10
-  # * WBDHU12		 
-  # * WBDHU14
-  # * WBDHU16
-
-HUC2 <- readOGR(gdb, "WBDHU2")
-# str (HUC2)
-
-# Interested in only the @data, and of them, the fields:huc2, states, name
-
-# Write CSV Location
-setwd("../HUC-Data/")
-
-keepsHUC2 <- c("huc2","states","name")
-HUC2List <- HUC2@data[keepsHUC2]
-# str (HUC2List)
-rm (HUC2)
-write.csv (HUC2List, "HUC2List.csv")
-
-# Do similar for HUC4,etc
-HUC4 <- readOGR(gdb, "WBDHU4")
-keepsHUC4  <- c("huc4","states","name")
-HUC4List <- HUC4@data[keepsHUC4]
-rm (HUC4)
-write.csv (HUC4List, "HUC4List.csv")
-
-
-
-HUC6 <- readOGR(gdb, "WBDHU6")
-keepsHUC6  <- c("huc6","states","name")
-HUC6List <- HUC6@data[keepsHUC6]
-rm(HUC6)
-write.csv (HUC6List, "HUC6List.csv")
-
-HUC8 <- readOGR(gdb, "WBDHU8")
-keepsHUC8  <- c("huc8","states","name")
-HUC8List   <- HUC8@data[keepsHUC8]
-rm (HUC8)
-write.csv (HUC8List, "HUC8List.csv")
-
-HUC10 <- readOGR(gdb, "WBDHU10")
-keepsHUC10 <- c("huc10","states","name")
-HUC10List  <- HUC10@data[keepsHUC10]
-rm (HUC10)
-write.csv (HUC10List, "HUC10List.csv")
-
-HUC12 <- readOGR(gdb, "WBDHU12")
-keepsHUC12 <- c("huc12","states","name")
-HUC12List <- HUC12@data[keepsHUC12]
-rm (HUC12)
-write.csv (HUC12List, "HUC12List.csv")
-
-HUC14 <- readOGR(gdb, "WBDHU14")
-keepsHUC14 <- c("huc14","states","name")
-HUC14List <- HUC14@data[keepsHUC14]
-rm(HUC14)
-write.csv (HUC14List, "HUC14List.csv")
-
-HUC16 <- readOGR(gdb, "WBDHU16")
-keepsHUC16 <- c("huc16","states","name")
-HUC16List <- HUC16@data[keepsHUC16]
-rm(HUC16)
-write.csv (HUC16List, "HUC16List.csv")
 
 
 
